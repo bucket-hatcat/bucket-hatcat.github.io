db51206f766a9f0aa149cf819b320f433c927008
Lauren Wierzbicki <lauren@laurenwierzbicki.com>
2021-10-14 16:20:34 -0700

    i have commit issues

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
 console.log('hi')
 
-// select the button
-    //add clicl event for when button is clicked
-       $('#kanye').on('click', function(){
-//use ajax to request info         
+
+       $('#kanye').on('click', function(){      
         $.ajax({
                 method: 'get',
                 url: 'https://api.kanye.rest/'
             })      
-    //successful add item
           .then(response => {
               console.log(response.quote)
 
               $('#quote').text(response.quote) 
       })
     })
-   
-  
